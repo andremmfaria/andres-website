aa07a56667158171f7580ece61a2e28d531bd085
Simone Ferreira <simoneef1986@gmail.com>
2020-04-15 16:45:33 +0100

    main page update

--- js/style.js
+++ js/style.js
@@ -1,8 +1,15 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
     } else {
-      x.style.display = "block";
+      content.style.maxHeight = content.scrollHeight + "px";
     }
-  }
+  });
+}
